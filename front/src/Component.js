0b6f29a49d3d55fd75239dffbf7ca8145a388a18
Borenko Anastasia <905-bad19u141@users.noreply.git.iu7.bmstu.ru>
2023-05-03 12:47:59 +0300

    feat: add example component

---
+++ front/src/Component.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Component.css';
+
+class Timer extends Component {
+  render() {
+    return (
+      <div class='div-1'>
+          <p>
+            КриптоАфиша
+          </p>
+      </div>
+    );
+  }
+}
+
+export default Timer;
